291acbc65d220a9ae623f05973932e198d0f116c
IMRMT <michaeltimonuli@gmail.com>
2024-02-22 14:34:59 +0700

    comment di LoginActivity

--- app/src/main/java/com/example/anmp_week1/LoginActivity.kt
+++ app/src/main/java/com/example/anmp_week1/LoginActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class LoginActivity : AppCompatActivity() {
+    //I Made Rai Michael Timonuli (160421056) Commented
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
