4b7f2099847799ce55530770aad845d1e07e70ad
IMRMT <michaeltimonuli@gmail.com>
2024-02-22 14:34:59 +0700

    comment di LoginActivity

--- app/src/main/java/com/example/anmp_week1/LoginActivity.kt
+++ app/src/main/java/com/example/anmp_week1/LoginActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class LoginActivity : AppCompatActivity() {
+    //I Made Rai Michael Timonuli (160421056) Commented
     override fun onCreate(savedInstanceState: Bundle?) {
         //test branch
         super.onCreate(savedInstanceState)
