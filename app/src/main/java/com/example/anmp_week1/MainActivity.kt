f36c4e3f5478962c118edb0262e4c4688fc54939
Daniel _ <danieltmx333@gmail.com>
2024-02-23 15:27:53 +0700

    + find bug

--- app/src/main/java/com/example/anmp_week1/MainActivity.kt
+++ app/src/main/java/com/example/anmp_week1/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    // bug
     // code baru di sini
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
