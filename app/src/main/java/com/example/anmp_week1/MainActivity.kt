0b8fb73fc3d4d18929b24feffa5ee3075ec1213b
Daniel _ <danieltmx333@gmail.com>
2024-02-23 15:14:13 +0700

    + bug MainActivity fixed

--- app/src/main/java/com/example/anmp_week1/MainActivity.kt
+++ app/src/main/java/com/example/anmp_week1/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // code baru di sini
+        // bug fixed here
     }
 }
