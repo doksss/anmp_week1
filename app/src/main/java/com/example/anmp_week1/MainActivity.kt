c5663c086c4e75a30c144b8545d6db3696f17882
Daniel _ <danieltmx333@gmail.com>
2024-02-23 15:28:48 +0700

    + find bug

--- app/src/main/java/com/example/anmp_week1/MainActivity.kt
+++ app/src/main/java/com/example/anmp_week1/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // code baru di sini
         // bug fixed here
+        // bug banget di sini
     }
 }
